7c1cd16e9fd35da2839b74da33cdbdc5d25869a6
Polina Kren <polina.kren8952@mail.ru>
2023-10-22 23:10:14 +0300

    Task-9: added brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainPrimeGame from '../src/games/brain-prime.js';
+
+brainPrimeGame();
---
+++ src/games/brain-prime.js
@@ -0,0 +1,25 @@
+import main from '../index.js';
+import generateRandomNumber from '../utils.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const minRange = 1;
+const maxRange = 50;
+
+const isPrime = (num) => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+        if (num % i === 0) return false;
+    }
+    return num > 1;
+};
+
+const beginRound = () => {
+    const randomNumber = generateRandomNumber(minRange, maxRange);
+    const question = randomNumber.toString();
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+    return [question, correctAnswer];
+};
+
+const brainPrimeGame = () => main(rule, beginRound);
+
+export default brainPrimeGame;
