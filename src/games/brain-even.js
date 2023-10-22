7a316ba0b47533173f74c05b9ec5444bd96f69e1
Polina Kren <polina.kren8952@mail.ru>
2023-10-22 23:10:38 +0300

    Refactoring: brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,13 +2,16 @@ import main from '../index.js';
 import generateRandomNumber from '../utils.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+const minRange = 1;
+const maxRange = 100;
 
 const isEven = (number) => number % 2 === 0;
 
 const beginRound = () => {
-  const randomNumber = generateRandomNumber(1, 100);
+  const randomNumber = generateRandomNumber(minRange, maxRange);
   const question = randomNumber.toString();
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
   return [question, correctAnswer];
 };
 
