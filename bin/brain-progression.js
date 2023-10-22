667070a4573ab1429c835c3489203f41a8d3d1d7
Polina Kren <polina.kren8952@mail.ru>
2023-10-22 22:57:50 +0300

    Task-8: added brain-progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainProgressionGame from '../src/games/brain-progression.js';
+
+brainProgressionGame();
---
+++ src/games/brain-progression.js
@@ -0,0 +1,32 @@
+import main from '../index.js';
+import generateRandomNumber from '../utils.js';
+
+const rule = 'What number is missing in the progression?';
+const minLength = 5;
+const maxLength = 10;
+const minRange = 1;
+const maxRange = 100;
+const minStep = 1;
+const maxStep = 10;
+
+const getProgression = (start, step, length, hiddenIndex) => {
+  const progression = Array(length).fill(0).map((_, i) => start + (step * i));
+  progression[hiddenIndex] = '..';
+  return progression.join(' ');
+};
+
+const beginRound = () => {
+  const startNumber = generateRandomNumber(minRange, maxRange);
+  const step = generateRandomNumber(minStep, maxStep);
+  const length = generateRandomNumber(minLength, maxLength);
+  const hiddenIndex = generateRandomNumber(2, length - 1);
+
+  const question = getProgression(startNumber, step, length, hiddenIndex);
+  const correctAnswer = (startNumber + step * hiddenIndex).toString();
+
+  return [question, correctAnswer];
+};
+
+const brainProgressionGame = () => main(rule, beginRound);
+
+export default brainProgressionGame;
